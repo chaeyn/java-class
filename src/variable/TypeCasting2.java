40597a775398e0df20cec88ea9c5f8b5acd1db08
chaeyn <chaulfe@gmail.com>
2026-03-04 15:10:14 +0900

    260304 자바란, 변수, 자료형, 형변환

---
+++ src/variable/TypeCasting2.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class TypeCasting2 {
+    public static void main(String[] args) {
+        long value5 = 10L;
+        long value6 = 10l;
+        float value7 = 10F;
+        double value8 = 10f;
+
+        System.out.println("value1 = " + value5);
+        System.out.println("value2 = " + value6);
+        System.out.println("value3 = " + value7);
+        System.out.println("value4 = " + value8);
+    }
+}
---
+++ src/variable/Var3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a = 100;
+        double b = 10.5;
+        boolean c = true;
+        char d = 'A';
+        String e = "Hello Java";
+    }
+}
