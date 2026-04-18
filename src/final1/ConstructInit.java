6b455e8081a9c62f1a8bb0b0cd92e5aa49d6f37e
chaeyn <chaulfe@gmail.com>
2026-04-18 22:18:35 +0900

    240416 final

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,13 @@
+package final1;
+
+public class ConstructInit {
+    private final int value;
+
+    public ConstructInit(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
