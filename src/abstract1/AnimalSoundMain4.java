6fbf753329f11eb0dd56c94ae09e840a45bb3a0d
chaeyn <chaulfe@gmail.com>
2026-04-22 15:19:03 +0900

    260422 추상 클래스

---
+++ src/abstract1/AnimalSoundMain4.java
@@ -0,0 +1,26 @@
+package abstract1;
+
+import poly3.Animal;
+
+public class AnimalSoundMain4 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+        Pig pig = new Pig();
+
+        AbstractAnimal[] animalArr = {dog, cat, caw, pig};
+
+        for (AbstractAnimal animal : animalArr) {
+            soundAnimal(animal);
+        }
+
+//        AbstractAnimal abstractAnimal = new AbstractAnimal();
+    }
+
+    private static void soundAnimal(AbstractAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
